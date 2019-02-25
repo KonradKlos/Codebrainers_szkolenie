d749e5d15ba92bfb79d1a94bbba4f9004084277c
KonradKlos <klosconrad@gmail.com>
2019-02-25 20:33:35 +0100

    Wypisanie zmiennych i podstawowych typów danych

---
+++ python/0_2dzien/warunki.py
@@ -0,0 +1,13 @@
+zmienna1 = 5
+zmienna2 = 3.00
+zmienna3 = "Kot"
+liczby = [1,2,3,4,5,10,9,8,7,6]
+zmienna4 = True #bool boolean
+zmienna5 = None
+
+print(zmienna1)
+print(zmienna2)
+print(zmienna3)
+print(zmienna4)
+print(zmienna5)
+print(liczby)
