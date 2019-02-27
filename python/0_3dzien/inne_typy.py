52f3ead4b56a28f28cc12b24635d5565e87bc265
KonradKlos <klosconrad@gmail.com>
2019-02-27 20:56:51 +0100

    KROTKA (TUPLE) , SLOWNIK (DICT) , ZBIOR (SET)

---
+++ python/0_3dzien/inne_typy.py
@@ -0,0 +1,18 @@
+#stringow nie da sie zmieniac! 
+
+
+krotka = (1, 2, 3, 4, 5) #tuple , jest nie zmiennna. Nie da sie zmienic wartosci w tupli
+print(krotka[0])
+
+#dict , elementy w slowniku nie maja przypisanej kolejnosci. 
+#Slownik zawiera klucze i odpowiadajace im wartosci. 
+#Klucze sa niezmienne, natomiast wartosci moga byc dowolne.
+slownik = {
+    "haslo": "definicja",
+    "amendment": "poprawka",
+    (3, 14): "pi"
+}
+
+#set kolejnosc elementow jest nieokreslona, elementy w zbiorze sie nie powtarzaja. 
+#Elementy w zbiorze sa unikatowe, niepowtarzalne. Kolejnosc ich jest niezdefiniowana.
+zbior = {1,2,3,2}
