b361496520adb4907fc772d06b4023a934b24656
KonradKlos <klosconrad@gmail.com>
2019-03-02 16:38:55 +0100

    Tworzenie własnych funkcji. Funkcja iloczyn.

---
+++ python/0_4dzien/iloczyn.py
@@ -0,0 +1,10 @@
+#################
+#1. Nazwa: iloczyn
+#2. argumenty: a,b,c,d
+#3. zwraca: wynik mnożenia wszystkich argumentów.
+
+def iloczyn (a,b,c,d):
+    wynik = a*b*c*d
+    return wynik
+wynik = iloczyn(1,2,3,4) # w tym momencie dane podstawiają się do funkcji. 
+print(wynik)
