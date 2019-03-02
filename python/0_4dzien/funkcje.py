e14862ffd4a989ddc3957c5dc3b5757de87590b2
KonradKlos <klosconrad@gmail.com>
2019-03-02 16:24:50 +0100

    Tworzenie własnej funkcji.

---
+++ python/0_4dzien/funkcje.py
@@ -0,0 +1,7 @@
+def fibonacci(m):
+    #lista = [0,1] # m to numer elementu ciągu który trzeba wyliczyć.
+    print(m+1) # na sam koniec wypisuje to m.
+    return m+2 # funkcja sie zakańcza i zwraca wartość w tym miejscu.  
+
+wartosc = fibonacci(15) # tak wywołujemy daną funkcję, gdzie program wraca do początku podstawiając (w tym wypadku) liczbę 15 i funkcja się wykonuje.
+print(wartosc) #używając return na końcu naszej funkcji zapisuje się ona pod założoną zmienną.
