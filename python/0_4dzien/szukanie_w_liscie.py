17a1946f57d1d276c5c2269182758844f98c00c1
KonradKlos <klosconrad@gmail.com>
2019-03-02 10:16:28 +0100

    Szukanie w liscie za pomocą pętli for wypisując dodatkowo linię znalezionego rekordu.

--- python/0_4dzien/szukanie_w_liscie.py
+++ python/0_4dzien/szukanie_w_liscie.py
@@ -24,4 +24,17 @@ dziennik = [
 if uczen in dziennik: #przechodząc przez wszystkich w dzienniku, wszyscy będą przypisywani do zmiennej
         print("Znalazłem")
 
-  
+# trzeci program, gdzie enum pokazuje linię w której znaleziono dane.
+uczestnik = 'Piotr'
+dziennik = [
+    'Piotr',
+    'Konrad',
+    'Mateusz',
+    'Przemek',
+    'Olga',
+]
+    
+for pozycja, uczen in enumerate(dziennik):
+    if uczen == uczestnik:
+        print(pozycja, ": ", uczen)
+        
