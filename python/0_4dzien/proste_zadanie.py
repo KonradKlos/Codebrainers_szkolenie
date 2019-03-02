2de2454dd28c8f35a58056d464955e01af7b8d58
KonradKlos <klosconrad@gmail.com>
2019-03-02 11:21:10 +0100

    Zadanie sprawdzające podzielność liczb z listy.

---
+++ python/0_4dzien/proste_zadanie.py
@@ -0,0 +1,8 @@
+lista = range(101)
+for zmienna in lista:
+    if zmienna %15 == 0:
+        print(zmienna, "FIZZBUZZ")
+    if zmienna %5 == 0:
+        print(zmienna, "BUZZ")
+    if zmienna %3 == 0:
+        print(zmienna, "FIZZ")
