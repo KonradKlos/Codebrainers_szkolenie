a7a120c2d98b7eaa462f8037791f91a6d47a32ba
KonradKlos <klosconrad@gmail.com>
2019-03-02 14:03:22 +0100

    Obliczanie średniej wartości na podstawie elementów z listy.

--- python/0_4dzien/srednia_wartosc.py
+++ python/0_4dzien/srednia_wartosc.py
@@ -7,3 +7,16 @@ for liczba in lista:
     suma = suma + liczba
 print("Średnia wartość dla całej listy to: ", suma/i) 
 #Dzieli zapisaną sumę przez liczbę elementów w liście. Liczba elementów zapisana jest w zmiennej i.
+
+
+#zmienna = zmienna + x , tą samą deklarację można zapisać w sposób zamieszczony poniżej.
+#zmienna += x
+
+#zmienna2 = zmienna2 * x , tą samą deklarację można zapisać w sposób zamieszczony poniżej.
+#zmienna2 *= x
+
+#zmienna3 = zmienna3 / x
+#zmienna3/= zmienna 3
+
+#zmienna = zmienna ** (1/n) - pierwiastkowanie
+
