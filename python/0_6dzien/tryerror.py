80ff9f599a32448ccba563ccda8d9e3eba95fa0c
KonradKlos <klosconrad@gmail.com>
2019-03-06 18:51:21 +0100

    Program z zamierzonymi błędami w programie, aby wywołać w nich błędy. W przypadku błędu, program zwraca informację o błędzie.

---
+++ python/0_6dzien/tryerror.py
@@ -0,0 +1,31 @@
+try:
+    a = 1   
+    b = 0
+    c = a/b
+    print(c)
+except ZeroDivisionError:
+    print("Nie dziel przez zero!")
+    
+try:
+    lista = [22222222]
+    print(lista[123])
+except IndexError:
+    print("Błędny parametr zadeklarowanej listy!")
+
+try:
+    slownik = ('Konrad', 'Bartek')
+    print(slownik('Grzesiek'))
+except TypeError:
+    print("Nie możesz modyfikować tupli!")
+
+try:
+    proba(6)
+    print(proba())
+except NameError:
+    print("Nazwa nie została zdefiniowana")
+
+try:
+    lista.append()
+    print(lista())
+except TypeError:
+    print("Funkcja dodawania listy potrzebuje co najmniej jednego argumenty. Nie wpisałeś żadnego.")
