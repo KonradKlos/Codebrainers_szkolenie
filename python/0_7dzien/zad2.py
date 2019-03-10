bf5262a5773f9924314bfbeee27c41e32eb0db78
KonradKlos <klosconrad@gmail.com>
2019-03-10 14:11:29 +0100

    Kolejny program tworzący plik tekstowy i wpisujący do niego zadaną treść.

--- python/0_7dzien/zad2.py
+++ python/0_7dzien/zad2.py
@@ -1,3 +1,10 @@
+#open(NAZWA_PLIKU, TRYB)
+#NAZWA_PLIKU -- string
+# TRYB -- string ('r', 'w', 'a', ew. z dodatkowym znakiem 't' lub 'b')
+# (np. 'rt', 'wb', etc.)
+# r - read, w - write, a - append, t - text, b - bytes 
+
+
 f = open('nowy_plik.txt', 'w')
 f.write('To zostanie wpisane do pliku.')
 f.close()
