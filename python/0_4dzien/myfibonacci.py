4c401f3a31c87c2f584af6576095de7629cadc1c
KonradKlos <klosconrad@gmail.com>
2019-03-10 13:53:38 +0100

    Aktualizacja programu.

--- python/0_4dzien/myfibonacci.py
+++ python/0_4dzien/myfibonacci.py
@@ -1,9 +1,26 @@
 
 def fibonacci(m):
+    #wymagania m:
+    # 1. to musi być liczba
+    # 2. liczba >= 2
+    if not isinstance(m,int):
+        raise Exception("To nie jest int")
+    if m < 2:
+        raise Exception("Liczba musi być większa od 1")
     lista = [0,1] 
     for i in range(2, m+1):
         lista.append(lista[i-1] + lista[i-2])
     mtyelement = lista[-1]
     return mtyelement
-mty = fibonacci(10)
-print(mty)
+try:
+    mty = fibonacci(10)
+    print(mty)
+except Exception:
+    print("Liczba musi być równa bądź większa od 2!")
+
+#
+#try:
+#    print(fib(1))
+#except Exception as exc:
+#    print("Wystapil blad: ")
+#    print(exc.args[0])
