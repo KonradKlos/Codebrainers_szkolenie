5efb02b3c80b0e0c436f5fe7bd5ccba63910b9d9
KonradKlos <klosconrad@gmail.com>
2019-03-10 14:56:23 +0100

    Program czytający plik csv.

---
+++ python/0_7dzien/pliki_csv2.py
@@ -0,0 +1,9 @@
+import csv 
+
+with open ('przykladowy.csv', 'r') as f:
+    czytacz = csv.DictReader(f)
+    for wiersz in czytacz:
+        print(wiersz)
+        print (wiersz['Year'])
+        print(wiersz['Score'])
+        print(wiersz['Title'])
