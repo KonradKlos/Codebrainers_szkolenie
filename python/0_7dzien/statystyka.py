ac4c7e2b27ee701f0d159c17b744b28dce7c85e1
KonradKlos <klosconrad@gmail.com>
2019-03-10 14:51:13 +0100

    Program statystyczny bazujący na pliku csv.

---
+++ python/0_7dzien/statystyka.py
@@ -0,0 +1,25 @@
+import csv
+
+years = []
+scores = []
+
+with open ('przykladowy.csv', 'r') as f:
+    czytacz = csv.DictReader(f)
+    for wiersz in czytacz:
+        #1 Konwertowanie na int
+        year = int(wiersz['Year'])
+        score = int(wiersz['Score'])
+
+        #2 Dodawanie do listy
+        years.append(year)
+        scores.append(score)
+
+        #3 Wypisz każdą listę, by można było sprawdzić
+minimalny_wynik = scores[0]
+minimalny_wynik_rok = years[0]
+maksymalny_wynik = scores[0]
+maksymalny_wynik_rok = years[0]
+
+for rok, wynik in zip(years, scores):
+    print('W roku', rok, 'miał wynik', wynik)
+    # czy ten wynik jest mniejszy od najmniejszygo dotychczasowego wyniku?
