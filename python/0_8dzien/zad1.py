41c85edade9b266c797730a0fa8e0933314ffddd
KonradKlos <klosconrad@gmail.com>
2019-03-11 18:56:24 +0100

    Testowanie docstringów.

--- python/0_8dzien/zad1.py
+++ python/0_8dzien/zad1.py
@@ -1,4 +1,9 @@
 def min (L):
-    "Opis mojej funkcji."
+    """Funkcja szukająca minimalnej wartości.
+    
+    Funkcja pobiera listę wartości i iteruje po nich,
+    żeby znaleźć najmniejszą.
+    """
     pass
 print(min.__doc__) #piąta linka wydrukuje nam parametr, który naywa sie podwójny podkreślnik..
+                    #double under - dunder, czyli podwójne znaki podkreślenia. 
