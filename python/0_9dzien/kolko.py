241787c5409682941e7bfe8bab771bf1c3b4d948
KonradKlos <klosconrad@gmail.com>
2019-03-13 20:25:04 +0100

    Rysowanie tablicy kolko krzyzyk.

---
+++ python/0_9dzien/kolko.py
@@ -0,0 +1,6 @@
+L = [["","",""],
+    ["","",""],
+    ["","",""]]
+wiersz = input("Podaj wiersz: ")
+kolumna = input("Podaj kolumnę: ")
+#skopiować rysowanie tablicy kolko krzyzyk z githuba Piotra.
