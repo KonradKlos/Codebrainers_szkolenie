7ba60fe2635bb86db0a67bab67a266b6015b94e8
KonradKlos <klosconrad@gmail.com>
2019-03-13 20:46:35 +0100

    Program kolko krzyzyk.

--- python/0_9dzien/kolko.py
+++ python/0_9dzien/kolko.py
@@ -1,6 +1,43 @@
 L = [["","",""],
     ["","",""],
     ["","",""]]
-wiersz = input("Podaj wiersz: ")
-kolumna = input("Podaj kolumnę: ")
+def rysuj_tablice(tablica):
+    for x, wiersz in enumerate(tablica):
+        for y, v in enumerate(wiersz):
+            if not v.strip():
+                v = ' '
+            if y < len(wiersz) - 1:
+                print(v, '| ', end='')
+            else:
+                print(v)
+        if x < len(tablica) - 1:
+            print('-' * 9)
+
+gracz = "o"
+for i in range(9):
+    print("Kolej gracza: ", gracz)
+    wiersz = int(input("Podaj wiersz: "))
+    kolumna = int(input("Podaj kolumnę: "))
+    L[wiersz][kolumna] = gracz
+    
+    rysuj_tablice(L)
+    if gracz == "o":
+        gracz = "x"
+    elif gracz == "x":
+        gracz = "o"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #skopiować rysowanie tablicy kolko krzyzyk z githuba Piotra.
