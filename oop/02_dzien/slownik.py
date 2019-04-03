c614400475871d6cf33752342844316f91d00f31
KonradKlos <klosconrad@gmail.com>
2019-04-03 18:51:02 +0200

    2 dzien.

---
+++ oop/02_dzien/slownik.py
@@ -0,0 +1,5 @@
+# słownik = {klucz : wartość for "cała składnia for"}
+
+tekst = "konrad klos"
+wystapienia = {znak: tekst.count(znak) for znak in tekst}
+print(wystapienia)
---
+++ oop/02_dzien/zad2zbior.py
@@ -0,0 +1,3 @@
+import random
+zbior = {random.randint(1,30) for _ in range (30)}
+print(len(zbior))
