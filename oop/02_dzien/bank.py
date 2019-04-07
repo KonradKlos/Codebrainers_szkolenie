819e06b1e90ca12922c4387ddb01132fb04e0b27
KonradKlos <klosconrad@gmail.com>
2019-04-07 14:08:56 +0200

    Program bankowy.

--- oop/02_dzien/bank.py
+++ oop/02_dzien/bank.py
@@ -8,13 +8,21 @@ class KontoBankowe:
         self.stan = stan
     
     def info(self):
-        print("Konto bankowe", self.nazwisko, "<", "stan:", self.stan, ">")
+        print("Konto bankowe", self.nazwisko, "<", "stan konta:", self.stan, ">")
     
     def wplac(self, kwota):
         self.stan += kwota
+        print("Dla konta",self.nazwisko,"wpłacono:", self.stan)
 
     def wyplac(self, kwota):
         self.stan -= kwota
+        print("Dla konta",self.nazwisko,"wypłacono:", self.stan)
+
+    def przelew (self, Wartosc_przelewu):
+        print("Wprowadź kwotę przelewu dla", self.nazwisko, ":") 
+        self.Wartosc_przelewu = (int(input()))
+        self.stan += self.Wartosc_przelewu
+
 
 
 jk = KontoBankowe ("Jan Kowalski")
@@ -24,7 +32,20 @@ jn = KontoBankowe ("Janusz Nowak", 1000)
 jk.info()
 jn.info()
 
+print("-------------------------------------------------")
+
 jk.wplac(2000)
 jk.wyplac(1500)
 
+print("-------------------------------------------------")
+print("Saldo po operacjach:")
 jk.info()
+jn.info()
+
+print("-------------------------------------------------")
+
+jk.przelew(0)
+jn.przelew(0)
+
+jk.info()
+jn.info()
